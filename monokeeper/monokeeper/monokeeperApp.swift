0e83319d4762dcbc21d10395085807d339728625
Matvey Kashin <matviy.kashin@gen.tech>
2025-02-16 14:15:27 +0200

    create initial project

---
+++ monokeeper/monokeeper/monokeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  monokeeperApp.swift
+//  monokeeper
+//
+//  Created by Matviy Kashin on 16.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct monokeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
